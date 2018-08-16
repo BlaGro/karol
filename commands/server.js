9877ab6ef46664f85ce0c0d32e6749459195c25d
djsklfhfhsjfhsji <41817988+BlaGro@users.noreply.github.com>
2018-08-16 09:39:42 +0200

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
   let server = message.guild.iconURL;
-  let embed = Discord.RichEmbed()
+  let embed =  new Discord.RichEmbed()
   .setDescription("Info o serwerze")
   .setColor("#647899")
   .setThumbnail(server)
