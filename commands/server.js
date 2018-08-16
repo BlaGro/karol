f1c3dc41c8992fb2f93a5144f34c3eb1c8bf97aa
djsklfhfhsjfhsji <41817988+BlaGro@users.noreply.github.com>
2018-08-16 09:31:42 +0200

    Add files via upload

---
+++ commands/server.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+  let server = message.guild.iconURL;
+  let embed = Discord.RichEmbed()
+  .setDescription("Info o serwerze")
+  .setColor("#647899")
+  .setThumbnail(server)
+  .addField("Właściciel", message.guild.owner)
+  .addField("Nazwa serwera", message.guild.name)
+  .addField("Kanał AFK", message.guild.afkChannel)
+  .addField("Koniec czasu AFK", message.guild.afkTimeout)
+  .addField("Powiadomienia o wiadomościach", message.guild.defaultMessageNotifications)
+  .addField("Domyślna rola", message.guild.defaultRole)
+  .addField("Możliwości", message.guild.features)
+  .addField("Ilość użytkowników", message.guild.memberCount)
+}
+module.exports.help = {
+  name:"server"
+}
